6654dd075ffd626a8859ca94f735ee347e9f9b31
Elena Williams <git@elena.net.au>
2023-07-01 15:08:32 +1000

    add early code egs

---
+++ example1/main.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+routes_eg1 = APIRouter(prefix="/example1")
+
+@routes_eg1.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@routes_eg1.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
