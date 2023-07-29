536f4adf02a230711390ab2e0bbb4eebc17344f8
Elena Williams <git@elena.net.au>
2023-07-29 21:45:32 +1000

    example 3: rename to match

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from fastapi.staticfiles import StaticFiles
 
 from example1.main import routes_eg1
 from example2.main import routes_eg2
+from example3.main import routes_eg3
 
 app = FastAPI()
 
 app.include_router(routes_eg1)
 app.include_router(routes_eg2)
+app.include_router(routes_eg3)
 
 app.mount('/', StaticFiles(directory='static', html=True))
 
--- example3/main.py
+++ example3/main.py
@@ -1,23 +1,34 @@
-from typing import Optional
+from typing import Literal, Optional
 
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-routes_eg3 = APIRouter(prefix="/example1")
+routes_eg3 = APIRouter(prefix="/example3")
+
+StateTerritory = Literal["ACT", "NSW", "NT", "OT", "QLD", "SA", "TAS", "VIC", "WA"]
 
 
-my_data = {
-    "street_number": 1,
-    "street_name": "Park",
-    "street_suffix": "Rd",
-}
-
-class MyModel(BaseModel):
-    unit_number: Optional[str]
-    street_number: int
+class FeatureProperty(BaseModel):
+    address_id: str
+    street_number: str
     street_name: str
-    street_suffix: str
+    locality: str
+    postcode: str
+    state: Literal[StateTerritory]
 
-@routes_eg3.get("/")
-async def basic_data(pk: Optional[int] = None):
-    return MyModel(**my_data)
+
+async def get_properties(address_id: str):
+    return {
+        "address_id": address_id,
+        "street_number": "101",
+        "street_name": "North Terrace",
+        "locality": "Adelaide",
+        "postcode": "5000",
+        "state": "ACT"
+    }
+
+
+@routes_eg3.get("/feature/properties")
+async def feature_properties(address_id: str = "GA1") -> FeatureProperty:
+    properties_kwargs: dict = await get_properties(address_id)
+    return FeatureProperty(**properties_kwargs)
